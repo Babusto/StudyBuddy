7204e3976630ab5f79bb47ff53970d98ce4c0656
Hussein Babu <husseinsalim419@gmail.com>
2023-09-21 23:44:36 +0300

    main

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
@@ -14,8 +15,17 @@ from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q=request.GET.get('q') if request.GET.get('q') !=None else ''
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name_icontains=q) |
+        Q(description_icontains=q)
+    )
+
+    topics= Topic.objects.all()
+
+    context = {'rooms': rooms, 'topics': topics}
     return render(request, 'base/home.html', context )
 
 def room(request, pk):
@@ -46,5 +56,12 @@ def updateRoom(request, pk):
             return redirect('home')
 
 
-    context ={}
+    context ={'form': form}
     return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room= Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html',{'obj':room})
