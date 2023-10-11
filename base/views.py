29f217dcffd1d04e07d936e13c3ed7190e09b84c
Hussein Babu <husseinsalim419@gmail.com>
2023-10-11 18:19:54 +0300

    Delete room

--- base/views.py
+++ base/views.py
@@ -92,6 +92,9 @@ def room(request, pk):
             room=room,
             body=request.POST.get('body')
         )
+
+        room.participants.add(request.user)
+
         return redirect('room', pk=room.id)
     context = {'room': room, 'room_messages': room_messages, 'participants':participants }
     return render(request, 'base/room.html', context )
@@ -136,3 +139,14 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html',{'obj':room})
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message= Message.objects.get(id=pk)
+    if request.user !=message.user:
+        return HttpResponse('You are not allowed here!!')
+    
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html',{'obj':message})
